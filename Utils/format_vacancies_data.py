6c9dd0897dbeb07c45b8741b63da462f50e8f6c7
Maxim Abavi <afghan1363@ya.ru>
2023-10-15 03:49:54 +0500

    функция format_vacancies_data

---
+++ Utils/format_vacancies_data.py
@@ -0,0 +1,21 @@
+
+def format_vacancies_data(answer_data):
+    vacancy_data = {'employers': [],
+                    'vacancies': []}
+    for data in answer_data:
+        vacancy_data['employers'].append({'employer_id': data['items'][0]['employer']['id'],
+                                          'employer_name': data['items'][0]['employer']['name'],
+                                          'employer_url': data['items'][0]['employer']['alternate_url'],
+                                          'vacancy_count': data['found']})
+        if not data['items'][0]['salary']:
+            data['items'][0]['salary'] = {}
+        if not data['items'][0]['snippet']['responsibility']:
+            data['items'][0]['snippet']['responsibility'] = data['items'][0]['name']
+        vacancy_data['vacancies'].append({'vacancy_id': data['items'][0]['id'],
+                                          'employer_id': data['items'][0]['employer']['id'],
+                                          'vacancy_title': data['items'][0]['name'],
+                                          'description': data['items'][0]['snippet']['responsibility'],
+                                          'salary_from': data['items'][0]['salary'].get('from'),
+                                          'salary_to': data['items'][0]['salary'].get('to'),
+                                          'vacancy_url': data['items'][0]['alternate_url']})
+    return vacancy_data
--- Utils/get_vacancies_data.py
+++ Utils/get_vacancies_data.py
@@ -3,14 +3,14 @@ import json
 import time
 
 
-def get_vacancies(emp_ids: list):
+def get_vacancies_data(emp_ids: list):
     answer_hh = []
     for emp_id in emp_ids:
         params = {
             'employer_id': emp_id,  # ID
             'area': 113,  # Поиск в зоне
             'page': 0,  # Номер страницы
-            'per_page': 30  # Кол-во вакансий на 1 странице
+            'per_page': 50  # Кол-во вакансий на 1 странице
         }
         req = requests.get('https://api.hh.ru/vacancies', params)
         data = req.content.decode()
